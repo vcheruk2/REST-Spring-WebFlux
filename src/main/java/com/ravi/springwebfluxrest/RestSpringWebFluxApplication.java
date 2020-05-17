27a58f6d4d0e63642fca4f321d415ae49dfdcb06
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-05-17 13:36:38 -0700

    Initial commit

---
+++ src/main/java/com/ravi/springwebfluxrest/RestSpringWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.springwebfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/springwebfluxrest/RestSpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.springwebfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
